2c4e637e69b1f7bfa531fc6d3d494ae7e8fce821
rialync <157668613+rialync@users.noreply.github.com>
2024-10-05 23:03:33 +0800

    Update Kernel.php

--- Middleware Exercise/app/Http/Kernel.php
+++ Middleware Exercise/app/Http/Kernel.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
@@ -8,27 +7,11 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        // Global HTTP middleware
-        \App\Http\Middleware\LogRequests::class, // Register LogRequests as global middleware
-    ];
-
-    protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \App\Http\Middleware\CheckAge::class, // Add CheckAge to web middleware group
-            
-        ],
-
-        'api' => [
-            'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        // Global middleware
+        \App\Http\Middleware\LogRequests::class, // Log all requests
     ];
 
     protected $routeMiddleware = [
-        'checkAge' => \App\Http\Middleware\CheckAge::class, // Register CheckAge middleware
+        'checkAge' => \App\Http\Middleware\CheckAge::class,
     ];
-    
 }
