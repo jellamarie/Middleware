56cd10a35b29762577a7c1ca2771af1adb937957
Jella <caceresjellamarie@gmail.com>
2024-10-08 01:19:00 +0800

    Docs

--- Middleware Exercise/resources/views/restricted-area.blade.php
+++ Middleware Exercise/resources/views/restricted-area.blade.php
@@ -15,17 +15,6 @@ ACCESS DENIED
 @section('main-class', 'main-content')
 
 @section('content')
-    <div class="blur-background"></div>
-
-    <div class="center-container">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100" class="circle-x-icon">
-            <!-- Outer Circle -->
-            <circle cx="50" cy="50" r="48" class="outer-circle" />
-            
-            <!-- X shape -->
-            <line x1="30" y1="30" x2="70" y2="70" class="cross-line" />
-            <line x1="70" y1="30" x2="30" y2="70" class="cross-line" />
-        </svg>
 
         <h1>Restricted Area</h1>
         <p>Welcome to the RESTRICTED AREA.</p>
